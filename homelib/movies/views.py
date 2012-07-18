276483031b79fec6eb8f73a7fedd85d4c39a91a5
epustovit <eugene.pustovit@gmail.com>
2012-07-18 23:05:53 +0300

    nice forms added

--- homelib/movies/views.py
+++ homelib/movies/views.py
@@ -3,19 +3,25 @@ from django.shortcuts import render_to_response, HttpResponseRedirect
 from movies.models import Movies
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from movies.forms import MovieAddingForm
 
 def index(request):
 	full_movies_list = Movies.objects.all()
-	movie = Movies.objects.get(pk = 1)
-	return render_to_response('movies/index.html', {'full_movies' : full_movies_list, 'movie' : movie})
-
-def add_movie_view(request):
-	return render_to_response('movies/add.html', context_instance=RequestContext(request))
+##	movie = Movies.objects.get(pk = 1)
+	return render_to_response('movies/index.html', {'full_movies' : full_movies_list})
 
 def add(request):
-	mov_name = request.POST.get('name')
-	mov_descr = request.POST.get('descr')
-	mov_gen = request.POST.get('genre')
-	new_movie = Movies(name = mov_name, description = mov_descr, genre = mov_gen)
-	new_movie.save()
-	return HttpResponseRedirect(reverse('movies.views.index'))
+	if request.method == 'POST':
+		form = MovieAddingForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name']
+			genre = form.cleaned_data['genre']
+			description = form.cleaned_data['description']
+			new_movie = Movies(name = name, genre = genre, description = description)
+			new_movie.save()
+			return HttpResponseRedirect('')
+	else:
+		form = MovieAddingForm()
+
+	return render_to_response('movies/add.html', { 'form' : form, }, context_instance=RequestContext(request))
+
