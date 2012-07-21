b38042f7b881da28085000235b9579ce4902683c
tetiana-pustovit <tat.parshina@gmail.com>
2012-07-21 23:12:55 +0300

    on the same page

--- homelib/movies/views.py
+++ homelib/movies/views.py
@@ -7,19 +7,16 @@ from movies.forms import MovieAddingForm
 
 def index(request):
 	full_movies_list = Movies.objects.all()
-<<<<<<< HEAD
-	movie = Movies.objects.get(pk = 1)
-	return render_to_response('base.html', context_instance=RequestContext(request))
+	return render_to_response('movies/index.html', {'full_movies' : full_movies_list})
 
 def navigate_to_movies_view(request):
 	return render_to_response('movies/movies.html', {'full_movies' : full_movies_list, 'movie' : movie})
 
 def add_movie_view(request):
 	return render_to_response('movies/add.html', context_instance=RequestContext(request))
-=======
-##	movie = Movies.objects.get(pk = 1)
-	return render_to_response('movies/index.html', {'full_movies' : full_movies_list})
->>>>>>> upstream/master
+
+
+	
 
 def add(request):
 	if request.method == 'POST':
